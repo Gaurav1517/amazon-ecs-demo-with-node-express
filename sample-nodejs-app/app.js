13895bbead1d41c6cb7bf72c10b4b1091d2d8be7
Gaurav1517 <gaurav.mau854@gmail.com>
2026-08-10 23:10:43 +0530

    adding code

--- sample-nodejs-app/app.js
+++ sample-nodejs-app/app.js
@@ -3,20 +3,28 @@
 const express = require("express");
 var bodyParser = require("body-parser");
 const app = express();
+
 app.use(
   bodyParser.urlencoded({
     extended: false,
   })
 );
 
+// --- HEALTH CHECK ROUTE ---
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "healthy" });
+});
+// --------------------------
+
 app.get("/", function (req, res) {
-  // console.log(__dirname) ;
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
-  num1 = Number(req.body.num1);
-  num2 = Number(req.body["num2"]);
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body["num2"]);
+  let operator;
+
   switch (req.body.operator) {
     case "+":
       operator = add;
@@ -31,20 +39,18 @@ app.post("/", function (req, res) {
       operator = divide;
       break;
   }
+
   console.log(calculator(num1, num2, operator));
-//   res.send(`<h2> the result is : ${calculator(num1, num2, operator)}</h2>` );
+
   res.send(`
     <h2>
-     That was easy, your result is: ${calculator(num1, num2, operator)}
+      That was easy, your result is: ${calculator(num1, num2, operator)}
     </h2>
-    <img src="https://image-repo-buraku.s3.eu-west-1.amazonaws.com/EC2.png" alt="EC2-Icon"class="center" width="10%" style="vertical-align:middle;margin:0px 100px">
-    <p>
-    There's no need for compliments </p>
+    <img src="https://image-repo-buraku.s3.eu-west-1.amazonaws.com/EC2.png" alt="EC2-Icon" class="center" width="10%" style="vertical-align:middle;margin:0px 100px">
+    <p>There's no need for compliments </p>
     <p>I already know i'm the smartest app in the world hahaha ;)</p>
-    <p>
-    By the way I'm runningg on a single EC2 Instance
-    </p>
-    <p><a href="YOUR IP">BACK TO CALCULATOR...</a></p>
+    <p>By the way I'm running on AWS Fargate now!</p>
+    <p><a href="/">BACK TO CALCULATOR...</a></p>
   `);
 });
 
@@ -63,10 +69,86 @@ function subtract(num1, num2) {
 function multiply(num1, num2) {
   return num1 * num2;
 }
+
 function divide(num1, num2) {
   return num1 / num2;
 }
+
 function calculator(num1, num2, operator) {
   return operator(num1, num2);
 }
+
+
+
+// // jshint esversion: 6
+
+// const express = require("express");
+// var bodyParser = require("body-parser");
+// const app = express();
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//   })
+// );
+
+// app.get("/", function (req, res) {
+//   // console.log(__dirname) ;
+//   res.sendFile(__dirname + "/index.html");
+// });
+
+// app.post("/", function (req, res) {
+//   num1 = Number(req.body.num1);
+//   num2 = Number(req.body["num2"]);
+//   switch (req.body.operator) {
+//     case "+":
+//       operator = add;
+//       break;
+//     case "x":
+//       operator = multiply;
+//       break;
+//     case "-":
+//       operator = subtract;
+//       break;
+//     case "/":
+//       operator = divide;
+//       break;
+//   }
+//   console.log(calculator(num1, num2, operator));
+// //   res.send(`<h2> the result is : ${calculator(num1, num2, operator)}</h2>` );
+//   res.send(`
+//     <h2>
+//      That was easy, your result is: ${calculator(num1, num2, operator)}
+//     </h2>
+//     <img src="https://image-repo-buraku.s3.eu-west-1.amazonaws.com/EC2.png" alt="EC2-Icon"class="center" width="10%" style="vertical-align:middle;margin:0px 100px">
+//     <p>
+//     There's no need for compliments </p>
+//     <p>I already know i'm the smartest app in the world hahaha ;)</p>
+//     <p>
+//     By the way I'm runningg on a single EC2 Instance
+//     </p>
+//     <p><a href="YOUR IP">BACK TO CALCULATOR...</a></p>
+//   `);
+// });
+
+// app.listen(3000, function () {
+//   console.log("server started on port 3000");
+// });
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// function subtract(num1, num2) {
+//   return num1 - num2;
+// }
+
+// function multiply(num1, num2) {
+//   return num1 * num2;
+// }
+// function divide(num1, num2) {
+//   return num1 / num2;
+// }
+// function calculator(num1, num2, operator) {
+//   return operator(num1, num2);
+// }
 
